be483782e7d4a7485710802aa2eb741fe8c8d9a7
mahjoubDev <mahjoubgit@gmail.com>
2015-05-03 22:15:51 +0100

    add angula js

---
+++ src/main/webapp/demo2/js/app.js
@@ -0,0 +1,28 @@
+var taskManagerModule = angular.module('taskManagerApp', ['ngAnimate']);
+
+taskManagerModule.controller('taskManagerController', function ($scope,$http) {
+	
+	var urlBase="http://localhost:8080/GestionResource/proxym/category";
+	 $scope.toggle='!toggle';	
+	$http.get(urlBase + '/getList').
+    	success(function(data) {
+	        $scope.tasks = data;
+    });
+	
+	//add a new task
+	$scope.addTask = function addTask() {
+		if($scope.categoryName=="" || $scope.categoryReference==""){
+			alert("Insufficient Data! Please provide values for category name and reference");
+		}
+		else{
+		 $http.post(urlBase + '/add' ,{  "nameCategorie": $scope.categoryName, "reference": $scope.categoryReference},{headers: {'Content-Type': 'application/json'} }).
+		  success(function(data) {
+			 alert("Category added");
+			 $scope.toggle='!toggle';
+			 $route.reload();
+		    });
+		}
+	};
+	
+});
+
